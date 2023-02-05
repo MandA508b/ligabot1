45fe2e75a8d7f804194440934a2db8b7a3b82bf2
Andriy Derkach <andriyderk@gmail.com>
2023-02-05 21:17:11 +0200

    new updare2

--- index.js
+++ index.js
@@ -15,9 +15,23 @@ bot.telegram.setMyCommands( [{ command: 'menu', description: 'Open menu buttons'
 bot.start(async (ctx) => {
     const registration = await userController.start(ctx.update.message.from)
     if(registration)
-        ctx.reply("Вітаю! Ви успішно зареєстровані як користувач!")
+        await ctx.reply("Вітаю! Ви успішно зареєстровані як користувач!", Markup
+            .keyboard([
+                ['Додати оголошення', 'Мої оголошення'],
+                ['Канали', 'Мої чати']
+            ])
+            .oneTime()
+            .resize()
+        )
     else
-        ctx.reply("Ви вже зареєстровані!")
+        await ctx.reply("Ви вже зареєстровані!", Markup
+            .keyboard([
+                ['Додати оголошення', 'Мої оголошення'],
+                ['Канали', 'Мої чати']
+            ])
+            .oneTime()
+            .resize()
+        )
 })
 
 
@@ -25,7 +39,14 @@ bot.hears('Канали', async (ctx)=>{
     const userAuth =  await userService.getUserByTelegramId(ctx.update.message.from.id)
     const accessToMenu = await userController.accessToMenu(ctx.update.message.from.id)
     if(userAuth.isBlocked || !accessToMenu){
-        return ctx.reply('У вас немає доступу!')
+        return await ctx.reply('У вас немає доступу!', Markup
+            .keyboard([
+                ['Додати оголошення', 'Мої оголошення'],
+                ['Канали', 'Мої чати']
+            ])
+            .oneTime()
+            .resize()
+        )
     }
 
     const user = await userService.getUserByTelegramId(ctx.update.message.from.id)
@@ -34,18 +55,39 @@ bot.hears('Канали', async (ctx)=>{
     for (let channelsKey in channels) {
         channelsList += `${channels[channelsKey].URL}\n`
     }
-    ctx.reply(channelsList)
+    await ctx.reply(channelsList, Markup
+        .keyboard([
+            ['Додати оголошення', 'Мої оголошення'],
+            ['Канали', 'Мої чати']
+        ])
+        .oneTime()
+        .resize()
+    )
 })
 
 bot.hears('Мої оголошення', async (ctx)=> {
     const userAuth =  await userService.getUserByTelegramId(ctx.update.message.from.id)
     const accessToMenu = await userController.accessToMenu(ctx.update.message.from.id)
     if(userAuth.isBlocked || !accessToMenu){
-        return ctx.reply('У вас немає доступу!')
+        return await ctx.reply('У вас немає доступу!', Markup
+            .keyboard([
+                ['Додати оголошення', 'Мої оголошення'],
+                ['Канали', 'Мої чати']
+            ])
+            .oneTime()
+            .resize()
+        )
     }
 
     const advertisements = await advertisementService.getAllByTelegramId(ctx.update.message.from.id)
-
+    await ctx.reply("Ваші оголошення: ", Markup
+        .keyboard([
+            ['Додати оголошення', 'Мої оголошення'],
+            ['Канали', 'Мої чати']
+        ])
+        .oneTime()
+        .resize()
+    )
     for (let advertisementsKey in advertisements) {
         const cityId = advertisements[advertisementsKey].cityId
         const cityName = await cityService.findById(cityId)
@@ -60,7 +102,14 @@ bot.hears('Мої оголошення', async (ctx)=> {
             Markup.inlineKeyboard([
                 Markup.button.webApp(`Редагувати`, `${process.env.ADVERTISEMENT_REDACT_URL}${advertisements[advertisementsKey]._id}`),
                 Markup.button.callback('Скасувати', 'delete')
-            ]))
+            ]), Markup
+                .keyboard([
+                    ['Додати оголошення', 'Мої оголошення'],
+                    ['Канали', 'Мої чати']
+                ])
+                .oneTime()
+                .resize()
+        )
     }
 })
 
@@ -74,13 +123,34 @@ bot.hears('Додати оголошення', async (ctx)=> {
     const userAuth =  await userService.getUserByTelegramId(ctx.update.message.from.id)
     const accessToMenu = await userController.accessToMenu(ctx.update.message.from.id)
     if(userAuth.isBlocked || !accessToMenu){
-        return ctx.reply('У вас немає доступу!')
+        return await ctx.reply('У вас немає доступу!', Markup
+            .keyboard([
+                ['Додати оголошення', 'Мої оголошення'],
+                ['Канали', 'Мої чати']
+            ])
+            .oneTime()
+            .resize()
+        )
     }
-
-    ctx.reply('Заповніть форму: ',
+    await ctx.reply("Заповніть форму:", Markup
+        .keyboard([
+            ['Додати оголошення', 'Мої оголошення'],
+            ['Канали', 'Мої чати']
+        ])
+        .oneTime()
+        .resize()
+    )
+    await ctx.reply('ФОРМА',
         Markup.inlineKeyboard([
             Markup.button.webApp('Заповнити', `${process.env.ADVERTISEMENT_CREATE_URL}`)
-        ]))
+        ]), Markup
+            .keyboard([
+                ['Додати оголошення', 'Мої оголошення'],
+                ['Канали', 'Мої чати']
+            ])
+            .oneTime()
+            .resize()
+    )
 
 })
 
@@ -88,7 +158,14 @@ bot.action('1', async (ctx)=> {
     const number = Number(ctx.update.callback_query.message.text.split(' ')[1].split('\n')[0].slice(1))
     const advertisement = await advertisementService.getByNumber(number)
 
-    if(!advertisement)return ctx.telegram.sendMessage(ctx.update.callback_query.from.id, "Щось пішло не так!")
+    if(!advertisement)return await ctx.telegram.sendMessage(ctx.update.callback_query.from.id, "Щось пішло не так!", Markup
+        .keyboard([
+            ['Додати оголошення', 'Мої оголошення'],
+            ['Канали', 'Мої чати']
+        ])
+        .oneTime()
+        .resize()
+    )
 
     const userClient = await userService.getUserByTelegramId(ctx.update.callback_query.from.id)
     const userCustomer = await userService.getUserById(advertisement.userId)
@@ -108,11 +185,25 @@ bot.action('1', async (ctx)=> {
 
     await ctx.telegram.sendMessage(userClient.telegramId,`Відповісти на замовлення №${number}`, Markup.inlineKeyboard([
         Markup.button.webApp(`Відповісти`, `${process.env.CHAT_URL}/chat?name=client&room=${chat.room}`),
-    ]))
+    ]), Markup
+        .keyboard([
+            ['Додати оголошення', 'Мої оголошення'],
+            ['Канали', 'Мої чати']
+        ])
+        .oneTime()
+        .resize()
+    )
 
     await ctx.telegram.sendMessage(userCustomer.telegramId, `Хтось хоче вам відповісти на замовлення №${number}`, Markup.inlineKeyboard([
-        Markup.button.webApp(`Відповісти`, `${process.env.CHAT_URL}/chat?name=customer&room=${chat.room}`),
-    ]))
+        Markup.button.webApp(`Відповісти`, `${process.env.CHAT_URL}/chat?name=customer&room=${chat.room}`)
+    ]), Markup
+        .keyboard([
+            ['Додати оголошення', 'Мої оголошення'],
+            ['Канали', 'Мої чати']
+        ])
+        .oneTime()
+        .resize()
+    )
 
 })
 
@@ -120,26 +211,61 @@ bot.hears('Мої чати', async (ctx)=>{
     const userAuth =  await userService.getUserByTelegramId(ctx.update.message.from.id)
     const accessToMenu = await userController.accessToMenu(ctx.update.message.from.id)
     if(userAuth.isBlocked || !accessToMenu){
-        return ctx.reply('У вас немає доступу!')
+        return await ctx.reply('У вас немає доступу!', Markup
+            .keyboard([
+                ['Додати оголошення', 'Мої оголошення'],
+                ['Канали', 'Мої чати']
+            ])
+            .oneTime()
+            .resize()
+        )
     }
 
     const user = await userService.getUserByTelegramId(ctx.update.message.from.id)
     const customerChats = await chatService.getAllByCustomerId(user._id)
-    if(customerChats.length)    await bot.telegram.sendMessage(ctx.update.message.from.id, "Писали вам :")
+    if(customerChats.length)    await bot.telegram.sendMessage(ctx.update.message.from.id, "Писали вам :", Markup
+        .keyboard([
+            ['Додати оголошення', 'Мої оголошення'],
+            ['Канали', 'Мої чати']
+        ])
+        .oneTime()
+        .resize()
+    )
 
     for (let chatsKey in customerChats) {
         const advertisement = await advertisementService.getById(customerChats[chatsKey].advertisementId)
         await bot.telegram.sendMessage(ctx.update.message.from.id, `Листування з оголошенням №${advertisement.number}`, Markup.inlineKeyboard([
-            Markup.button.webApp(`Написати`, `${process.env.CHAT_URL}/chat?name=customer&room=${customerChats[chatsKey].room}`)]))
+            Markup.button.webApp(`Написати`, `${process.env.CHAT_URL}/chat?name=customer&room=${customerChats[chatsKey].room}`)]), Markup
+            .keyboard([
+                ['Додати оголошення', 'Мої оголошення'],
+                ['Канали', 'Мої чати']
+            ])
+            .oneTime()
+            .resize()
+        )
     }
 
     const clientChats = await chatService.getAllByClientId(user._id)
-    if(clientChats.length)    await bot.telegram.sendMessage(ctx.update.message.from.id, "Писали ви :")
+    if(clientChats.length)    await bot.telegram.sendMessage(ctx.update.message.from.id, "Писали ви :", Markup
+        .keyboard([
+            ['Додати оголошення', 'Мої оголошення'],
+            ['Канали', 'Мої чати']
+        ])
+        .oneTime()
+        .resize()
+    )
 
     for (let chatsKey in clientChats) {
         const advertisement = await advertisementService.getById(clientChats[chatsKey].advertisementId)
         await bot.telegram.sendMessage(ctx.update.message.from.id, `Листування з оголошенням №${advertisement.number}`, Markup.inlineKeyboard([
-            Markup.button.webApp(`Написати`, `${process.env.CHAT_URL}/chat?name=client&room=${clientChats[chatsKey].room}`)]))
+            Markup.button.webApp(`Написати`, `${process.env.CHAT_URL}/chat?name=client&room=${clientChats[chatsKey].room}`)]), Markup
+            .keyboard([
+                ['Додати оголошення', 'Мої оголошення'],
+                ['Канали', 'Мої чати']
+            ])
+            .oneTime()
+            .resize()
+        )
     }
 })
 
@@ -154,7 +280,6 @@ bot.hears('/menu', async (ctx)=>{
     )
 })
 
-console.log((new Date(Date.now()+60*2*60*1000)).toString())
 
 startServer()
 bot.launch()
